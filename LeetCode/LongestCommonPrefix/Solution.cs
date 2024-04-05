98714877730fb091eea697a0e77dd5cc65600b79
KACPERBIALY\kacpe <kacper.bialy@dataedo.com>
2024-04-05 23:21:47 +0200

    Resolving // https://leetcode.com/problems/longest-common-prefix/

---
+++ LeetCode/LongestCommonPrefix/Solution.cs
@@ -0,0 +1,34 @@
+﻿using System.Text;
+
+namespace LeetCode.LongestCommonPrefix;
+
+// https://leetcode.com/problems/longest-common-prefix/
+public static class Solution
+{
+    public static string LongestCommonPrefix(string[] strs)
+    {
+        var prefixBuilder = new StringBuilder();
+
+        var min = strs.Min(s => s.Length);
+
+        for (var i = 0; i < min; i++)
+        {
+            var isCommon = true;
+            for (var j = 1; j < strs.Length; j++)
+            {
+                if (strs[j - 1][i] != strs[j][i])
+                {
+                    isCommon = false;
+                    break;
+                }
+            }
+
+            if (isCommon)
+                prefixBuilder.Append(strs[0][i]);
+            else
+                break;
+        }
+
+        return prefixBuilder.ToString();
+    }
+}
---
+++ LeetCodeTests/LongestCommonPrefix/LongestCommonPrefixTests.cs
@@ -0,0 +1,35 @@
+﻿using FluentAssertions;
+using LeetCode.LongestCommonPrefix;
+
+namespace LeetCodeTests.LongestCommonPrefix;
+
+public class LongestCommonPrefixTests
+{
+    [Fact]
+    public void LongestCommonPrefix_WhenCommonPrefixExists_ReturnsCorrectPrefix()
+    {
+        // arrange
+        string[] input = ["flower", "flow", "flight"];
+        
+        // act
+        var prefix = Solution.LongestCommonPrefix(input);
+
+        // assert
+        prefix.Should()
+            .Be("fl");
+    }    
+    
+    [Fact]
+    public void LongestCommonPrefix_WhenCommonPrefixNotExists_ShouldReturnEmptyPrefix()
+    {
+        // arrange
+        string[] input = ["dog","racecar","car"];
+        
+        // act
+        var prefix = Solution.LongestCommonPrefix(input);
+
+        // assert
+        prefix.Should()
+            .Be(string.Empty);
+    }
+}
