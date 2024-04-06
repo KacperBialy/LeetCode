5d584d198992070cec587007e8552d6e8e318bbf
KACPERBIALY\kacpe <kacper.bialy@dataedo.com>
2024-04-06 11:45:27 +0200

    Resolving https://leetcode.com/problems/find-the-index-of-the-first-occurrence-in-a-string

---
+++ LeetCode/FindTheIndexOfTheFirstOccurrenceInAString/Solution.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.RegularExpressions;
+
+namespace LeetCode.FindTheIndexOfTheFirstOccurrenceInAString;
+
+// https://leetcode.com/problems/find-the-index-of-the-first-occurrence-in-a-string
+public static class Solution
+{
+    public static int StrStr(string haystack,string needle)
+    {
+        var regex = new Regex(needle);
+        var match = regex.Match(haystack);
+        
+        return match.Success ? match.Index : -1;
+    }
+}
